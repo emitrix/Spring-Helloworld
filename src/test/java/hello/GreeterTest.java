380b3ca56e2200f210bb382dea7c81dfb110c5b1
Jaime Gaona <jaimegaona@gmail.com>
2018-04-12 08:37:38 +0000

    Maven greeter project first commit

---
+++ src/test/java/hello/GreeterTest.java
@@ -0,0 +1,23 @@
+package hello;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class GreeterTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    private Greeter greeter = new Greeter();
+    
+    @Test
+    public void greeterSaysHello()
+    {
+        assertThat( greeter.sayHello(), containsString("Hello") );
+    }
+}
